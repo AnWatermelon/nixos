d54f665af687d563fa96abd8a157a5d70186546d
Max Hilton <maxfhilton52@gmail.com>
2026-07-30 16:22:49 -0500

    fix home manager version

--- modules/features/base/default.nix
+++ modules/features/base/default.nix
@@ -3,6 +3,7 @@
     home.username = "maxfh";
     home.homeDirectory = "/home/maxfh";
     home.stateVersion = "26.05";
+    home.enableNixpkgsReleaseCheck = false;
     programs.home-manager.enable = true;
   };
 }
