8682faca03968fad67106be5ebd465cdd724a130
Max Hilton <maxfhilton52@gmail.com>
2026-07-30 16:23:05 -0500

    add nix-command and flakes

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -2,6 +2,7 @@
 {
   config.flake.modules.nixos.core = { ... }: {
     programs.zsh.enable = true;
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = "26.05";
   };
