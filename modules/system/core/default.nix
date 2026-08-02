6c215b6e03f93f6f8c19c24c8bfe582c456a4ff8
Max Hilton <maxfhilton52@gmail.com>
2026-08-01 19:24:17 -0500

    modules/system/core: enable automatic store gc and optimisation

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -1,9 +1,24 @@
-{ config, ... }:
 {
-  config.flake.modules.nixos.core = { ... }: {
+  flake.modules.nixos.core = {
     programs.zsh.enable = true;
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    nix = {
+      settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+
+      # Keep the store from growing without bound.
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
+      };
+      optimise.automatic = true;
+    };
+
     nixpkgs.config.allowUnfree = true;
+
     system.stateVersion = "26.05";
   };
 }
