7091a43f86684d54e3f18ed961a56aa78400fa0c
Max Hilton <maxfhilton52@gmail.com>
2026-08-01 19:24:17 -0500

    modules/features: derive home paths from home.username

--- modules/features/base/default.nix
+++ modules/features/base/default.nix
@@ -1,9 +1,14 @@
 {
-  flake.modules.homeManager.base = {
-    home.username = "maxfh";
-    home.homeDirectory = "/home/maxfh";
-    home.stateVersion = "26.05";
-    home.enableNixpkgsReleaseCheck = false;
-    programs.home-manager.enable = true;
-  };
+  flake.modules.homeManager.base =
+    { config, ... }:
+    {
+      home = {
+        username = "maxfh";
+        homeDirectory = "/home/${config.home.username}";
+        stateVersion = "26.05";
+        enableNixpkgsReleaseCheck = false;
+      };
+
+      programs.home-manager.enable = true;
+    };
 }
--- modules/features/git/default.nix
+++ modules/features/git/default.nix
@@ -1,16 +1,18 @@
 {
-  flake.modules.homeManager.git = {
-    programs.git = {
-      enable = true;
-      signing.signByDefault = true;
-      signing.key = "/home/maxfh/.ssh/id_ed25519.pub";
-      settings = {
-        gpg.format = "ssh";
-        user = {
-          name = "Max Hilton";
-          email = "maxfhilton52@gmail.com";
+  flake.modules.homeManager.git =
+    { config, ... }:
+    {
+      programs.git = {
+        enable = true;
+        signing.signByDefault = true;
+        signing.key = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+        settings = {
+          gpg.format = "ssh";
+          user = {
+            name = "Max Hilton";
+            email = "maxfhilton52@gmail.com";
+          };
         };
       };
     };
-  };
 }
