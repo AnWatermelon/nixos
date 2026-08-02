58d34b96486272306cdef609f3b2edb47c026f2d
Max Hilton <maxfhilton52@gmail.com>
2026-08-01 21:07:25 -0500

    oh my zsh

--- modules/features/zsh/default.nix
+++ modules/features/zsh/default.nix
@@ -1,5 +1,36 @@
 {
   flake.modules.homeManager.zsh = {
-    programs.zsh.enable = true;
+    programs.zsh = { 
+      enable = true;
+      shellAliases = {
+	sp = "spotatui";
+	cfg = "cd ~/.config/";
+	lg = "lazygit";
+	p = "cd ~/Projects";
+      };
+      oh-my-zsh = {
+	enable = true;
+	theme = "gnzh";
+      };
+      initExtra = ''
+        # yazi cd-on-exit wrapper
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          command yazi "$@" --cwd-file="$tmp"
+          if IFS= read -r -d $'\0' cwd < "$tmp"; then
+            [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+          fi
+          command rm -f -- "$tmp"
+        }
+
+        # re-run last command with sudo
+        fuck() { sudo $(fc -ln -3 | xargs); }
+
+        fastfetch
+
+        # LM Studio CLI
+        export PATH="$PATH:$HOME/.lmstudio/bin"
+      '';
+    };
   };
 }
