c1d9dbf18d176db5b2a8277e33a43572ffe74f7f
Max Hilton <maxfhilton52@gmail.com>
2026-08-02 20:32:26 -0500

    Start managing some arch dotfiles with home-manager

---
+++ modules/hosts/arch/default.nix
@@ -0,0 +1,14 @@
+{ config, inputs, ... }:
+{
+  flake.homeConfigurations.maxfh = inputs.home-manager.lib.homeManagerConfiguration {
+    pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    extraSpecialArgs = { inherit inputs; };
+    modules = [
+       config.flake.modules.homeManager.base
+       config.flake.modules.homeManager.terminal
+       config.flake.modules.homeManager.hyprland
+       config.flake.modules.homeManager.kitty
+       config.flake.modules.homeManager.zsh
+    ];
+  };
+}
