04d51b22e8257ff9957e4199001d2b127de001cb
Max Hilton <maxfhilton52@gmail.com>
2026-08-14 14:37:59 -0500

    direnv

--- modules/features/zsh/default.nix
+++ modules/features/zsh/default.nix
@@ -16,23 +16,25 @@
         theme = "gnzh";
       };
       initContent = ''
-        # yazi cd-on-exit wrapper
-        function y() {
-          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-          command yazi "$@" --cwd-file="$tmp"
-          if IFS= read -r -d $'\0' cwd < "$tmp"; then
-            [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
-          fi
-          command rm -f -- "$tmp"
-        }
+                # yazi cd-on-exit wrapper
+                function y() {
+                  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+                  command yazi "$@" --cwd-file="$tmp"
+                  if IFS= read -r -d $'\0' cwd < "$tmp"; then
+                    [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+                  fi
+                  command rm -f -- "$tmp"
+                }
 
-        # re-run last command with sudo
-        fuck() { sudo $(fc -ln -3 | xargs); }
+                # re-run last command with sudo
+                fuck() { sudo $(fc -ln -3 | xargs); }
 
-        fastfetch
+                fastfetch
 
-        # LM Studio CLI
-        export PATH="$PATH:$HOME/.lmstudio/bin:$HOME/.local/bin"
+                # LM Studio CLI
+                export PATH="$PATH:$HOME/.lmstudio/bin:$HOME/.local/bin"
+
+        	eval "$(direnv hook zsh)"
       '';
     };
   };
