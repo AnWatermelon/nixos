99fbf753bf82115ae4efedcbb3edc59a5169e301
Max Hilton <maxfhilton52@gmail.com>
2026-08-19 12:43:07 -0500

    init for fw13pinit for fw13p

---
+++ modules/hosts/fw13p/default.nix
@@ -0,0 +1,47 @@
+{
+  config,
+  inputs,
+  ...
+}:
+{
+  flake.nixosConfigurations.nixos-test = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = { inherit inputs; };
+    modules = [
+      ./_hardware-configuration.nix
+      config.flake.modules.nixos.terminal
+      { networking.hostName = "fw13p"; }
+      {
+        services.openssh = {
+          enable = true;
+          settings = {
+            PasswordAuthentication = false;
+            PermitRootLogin = "no";
+          };
+        };
+      }
+
+      config.flake.modules.nixos.boot
+      config.flake.modules.nixos.networking
+      config.flake.modules.nixos.locale
+      config.flake.modules.nixos.audio
+      config.flake.modules.nixos.xdg
+      config.flake.modules.nixos.users
+      config.flake.modules.nixos.core
+
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = { inherit inputs; };
+          users.maxfh.imports = [
+            config.flake.modules.homeManager.base
+            config.flake.modules.homeManager.cli
+            config.flake.modules.homeManager.desktop
+          ];
+        };
+      }
+    ];
+  };
+}
+
