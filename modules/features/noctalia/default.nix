11d88f469a9828f65ef0cd12ea5aa23f89ad62ba
Max Hilton <maxfhilton52@gmail.com>
2026-08-23 14:23:18 -0500

    more noctalia stuff

--- modules/features/noctalia/default.nix
+++ modules/features/noctalia/default.nix
@@ -1,19 +1,32 @@
 { inputs, ... }:
 {
   flake.modules.homeManager.noctalia =
-    { pkgs, ... }:
     {
-      home.packages = [
-        inputs.noctalia.packages.${pkgs.system}.default
-      ];
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      options.my.noctalia = {
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = inputs.noctalia.packages.${pkgs.system}.default;
+          description = "The noctalia package to install.";
+        };
+      };
 
-      xdg.configFile = {
-        "noctalia/settings.toml".source = ./settings.toml;
-        "noctalia/user-templates.toml".source = ./user-templates.toml;
-        "noctalia/templates/zsh-palette.conf".source = ./templates/zsh-palette.conf;
-        "noctalia/templates/nvim-base16.lua".source = ./templates/nvim-base16.lua;
-        "noctalia/templates/spotatui.yml".source = ./templates/spotatui.yml;
-        "noctalia/templates/Translucence.theme.css".source = ./templates/Translucence.theme.css;
+      config = {
+        home.packages = [ config.my.noctalia.package ];
+
+        xdg.configFile = {
+          "noctalia/settings.toml".source = ./settings.toml;
+          "noctalia/user-templates.toml".source = ./user-templates.toml;
+          "noctalia/templates/zsh-palette.conf".source = ./templates/zsh-palette.conf;
+          "noctalia/templates/nvim-base16.lua".source = ./templates/nvim-base16.lua;
+          "noctalia/templates/spotatui.yml".source = ./templates/spotatui.yml;
+          "noctalia/templates/Translucence.theme.css".source = ./templates/Translucence.theme.css;
+        };
       };
     };
 }
