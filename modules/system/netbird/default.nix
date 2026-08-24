6953104d52524d61508b7ed7ee9a56865ddf1390
Max Hilton <maxfhilton52@gmail.com>
2026-08-23 19:36:24 -0500

    formatter commit

--- modules/system/netbird/default.nix
+++ modules/system/netbird/default.nix
@@ -2,11 +2,12 @@
   flake.modules.nixos.netbird = { config, inputs, ... }: {
     imports = [ inputs.sops-nix.nixosModules.sops ];
 
-    sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-
-    sops.defaultSopsFile = ./secrets/netbird-setup-key.yaml;
-    sops.secrets."netbird-setup-key" = {
-      restartUnits = [ "netbird-wt0-login.service" ];
+    sops = {
+      age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+      defaultSopsFile = ./secrets/netbird-setup-key.yaml;
+      secrets."netbird-setup-key" = {
+        restartUnits = [ "netbird-wt0-login.service" ];
+      };
     };
 
     services.netbird.clients.wt0 = {
