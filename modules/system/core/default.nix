f952a95eae50da0b9a21530258897098f2a3707e
Max Hilton <maxfhilton52@gmail.com>
2026-08-27 15:32:40 -0500

    moving stuff around and enabling upower

--- modules/system/core/default.nix
+++ modules/system/core/default.nix
@@ -1,51 +1,35 @@
 {
-  flake.modules.nixos.core =
-    { pkgs, ... }:
-    {
-      programs.zsh.enable = true;
+  flake.modules.nixos.core = _: {
+    nix = {
+      settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
 
-      nix = {
-        settings.experimental-features = [
-          "nix-command"
-          "flakes"
-        ];
-
-        gc = {
-          automatic = true;
-          dates = "weekly";
-          options = "--delete-older-than 30d";
-        };
-        optimise.automatic = true;
+      gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 30d";
       };
-
-      hardware.graphics = {
-        enable = true;
-        enable32Bit = true;
-      };
-
-      nixpkgs.config.allowUnfree = true;
-
-      programs.git = {
-        enable = true;
-        config.safe.directory = [ "/etc/nixos" ];
-      };
-
-      system.stateVersion = "26.05";
-
-      systemd.services.lock-root = {
-        description = "Lock the root account after the final system's first boot";
-        wantedBy = [ "multi-user.target" ];
-        unitConfig.ConditionPathExists = [
-          "/etc/root-lock-pending"
-          "!/var/lib/root-locked"
-        ];
-        serviceConfig.Type = "oneshot";
-        path = [ pkgs.shadow ];
-        script = ''
-          passwd -l root
-          rm -f /etc/root-lock-pending
-          touch /var/lib/root-locked
-        '';
-      };
+      optimise.automatic = true;
     };
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    services.upower.enable = true;
+    programs.zsh.enable = true;
+
+    nixpkgs.config.allowUnfree = true;
+
+    programs.git = {
+      enable = true;
+      config.safe.directory = [ "/etc/nixos" ];
+    };
+
+    system.stateVersion = "26.05";
+
+  };
 }
