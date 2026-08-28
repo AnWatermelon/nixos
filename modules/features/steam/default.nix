da74e882d6cb652ef175d0ecd450a58e1e5cfd61
Max Hilton <maxfhilton52@gmail.com>
2026-08-28 18:07:10 -0500

    mangohud

--- modules/features/steam/default.nix
+++ modules/features/steam/default.nix
@@ -10,5 +10,8 @@
         dedicatedServer.openFirewall = true;
         localNetworkGameTransfers.openFirewall = true;
       };
+      environment.systemPackages = [
+        pkgs.mangohud
+      ];
     };
 }
