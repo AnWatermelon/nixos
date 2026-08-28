b332a061f3fdaa12cac6f28cca809c10c31cbf9f
Max Hilton <maxfhilton52@gmail.com>
2026-08-28 17:12:46 -0500

    that didn't do what I thought I did

--- modules/features/steam/default.nix
+++ modules/features/steam/default.nix
@@ -5,12 +5,6 @@
       remotePlay.openFirewall = true;
       dedicatedServer.openFirewall = true;
       localNetworkGameTransfers.openFirewall = true;
-      gamescopeSession = {
-        enable = true;
-        env = {
-          "DRI_PRIME" = 1;
-        };
-      };
     };
   };
 }
