254be6d159fb3098f65f51c7e472b0f0db15e90e
Max Hilton <maxfhilton52@gmail.com>
2026-09-02 10:28:24 -0500

    formatter and mount in user space
    fixes perms issues

--- modules/system/nas/default.nix
+++ modules/system/nas/default.nix
@@ -1,18 +1,17 @@
 {
-  flake.modules.nixos.nas = 
-  { pkgs, ... }: 
-  {
-    environment.systemPackages = [ pkgs.cifs-utils ];
-    fileSystems."/mnt/share" = {
-      device = "//10.1.0.2/NAS/";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  flake.modules.nixos.nas =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = [ pkgs.cifs-utils ];
+      fileSystems."/home/maxfh/shared" = {
+        device = "//10.1.0.2/NAS/";
+        fsType = "cifs";
+        options =
+          let
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-        in
-        [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+          in
+          [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+      };
     };
-  };
 }
