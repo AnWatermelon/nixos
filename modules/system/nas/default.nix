b4ec4a17d75929c95977dde5d9e474bcef1bd54d
Max Hilton <maxfhilton52@gmail.com>
2026-09-02 11:11:14 -0500

    encrypt and inject smb secrets

--- modules/system/nas/default.nix
+++ modules/system/nas/default.nix
@@ -1,8 +1,22 @@
 {
   flake.modules.nixos.nas =
-    { pkgs, ... }:
     {
+      config,
+      inputs,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ inputs.sops-nix.nixosModules.sops ];
       environment.systemPackages = [ pkgs.cifs-utils ];
+
+      sops = {
+        age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        secrets."smb-credentials" = {
+          sopsFile = ./secrets/smb-credentials.yaml;
+        };
+      };
+
       fileSystems."/home/maxfh/shared" = {
         device = "//10.1.0.2/NAS/";
         fsType = "cifs";
@@ -11,7 +25,7 @@
             automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
           in
-          [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
+          [ "${automount_opts},credentials=${config.sops.secrets."smb-credentials".path}" ];
       };
     };
 }
