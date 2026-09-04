242cce9089db01ba960d0b91ee1a4fb95dd4ae09
Max Hilton <maxfhilton52@gmail.com>
2026-09-04 05:19:55 -0500

    perms update for nas

--- modules/system/nas/default.nix
+++ modules/system/nas/default.nix
@@ -22,7 +22,7 @@
         fsType = "cifs";
         options =
           let
-            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100";
 
           in
           [ "${automount_opts},credentials=${config.sops.secrets."smb-credentials".path}" ];
