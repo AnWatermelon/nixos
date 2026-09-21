4444dfeb96d713c18223d1ee8298da258d5006c2
Max Hilton <maxfhilton52@gmail.com>
2026-09-21 09:26:48 -0500

    rclone with vfs caching for NAS

--- modules/system/nas/default.nix
+++ modules/system/nas/default.nix
@@ -8,24 +8,53 @@
     }:
     {
       imports = [ inputs.sops-nix.nixosModules.sops ];
-      environment.systemPackages = [ pkgs.cifs-utils ];
+
+      programs.fuse.enable = true;
+      environment.systemPackages = [ pkgs.rclone ];
 
       sops = {
         age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-        secrets."smb-credentials" = {
-          sopsFile = ./secrets/smb-credentials.yaml;
+        secrets."rclone-nas-conf" = {
+          sopsFile = ./secrets/rclone-nas-conf.yaml;
+          owner = "maxfh";
         };
       };
 
-      fileSystems."/home/maxfh/shared" = {
-        device = "//10.1.0.2/NAS/";
-        fsType = "cifs";
-        options =
-          let
-            automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100";
+      systemd.tmpfiles.rules = [
+        "d /home/maxfh/shared 0755 maxfh users -"
+        "d /home/maxfh/.cache/rclone 0700 maxfh users -"
+      ];
 
-          in
-          [ "${automount_opts},credentials=${config.sops.secrets."smb-credentials".path}" ];
+      systemd.services.rclone-nas = {
+        description = "rclone mount of NAS share with local VFS cache";
+        after = [
+          "network-online.target"
+          "netbird-wt0.service"
+        ];
+        requires = [ "netbird-wt0.service" ];
+        wants = [ "network-online.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        path = [ "/run/wrappers" ];
+
+        environment.RCLONE_CONFIG = config.sops.secrets."rclone-nas-conf".path;
+
+        serviceConfig = {
+          Type = "notify";
+          User = "maxfh";
+          Group = "users";
+          ExecStart = ''
+            ${pkgs.rclone}/bin/rclone mount nas:NAS /home/maxfh/shared \
+              --vfs-cache-mode full \
+              --vfs-cache-max-age 720h \
+              --dir-cache-time 1h \
+              --cache-dir /home/maxfh/.cache/rclone \
+              --umask 022
+          '';
+          ExecStop = "/run/wrappers/bin/fusermount3 -u /home/maxfh/shared";
+          Restart = "on-failure";
+          RestartSec = 5;
+        };
       };
     };
 }
